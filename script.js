7ae8fb9cd19aaa39d443a8e4584398a6306376b9
kienchn <kiensalalima34@gmailcom>
2024-11-29 06:55:54 +0800

    Initial commit of my first someone

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "YAYYYYYY!! I LOVE YOUU SO MUCH MY BAYBEEEE 😘";
+  gif.src =
+    "https://gifdb.com/images/high/cute-love-baby-kiss-pw26tcs2lnig9xob.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
